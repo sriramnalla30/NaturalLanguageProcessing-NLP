ab2257a93ddf0b63e5145aa0c30904d1ce02bde2
sriramnalla30 <114870983+sriramnalla30@users.noreply.github.com>
2024-08-13 10:12:38 +0530

    Add files via upload

---
+++ ner-application/src/main/java/com/sriram/nerapplication/NerApplication.java
@@ -0,0 +1,13 @@
+package com.sriram.nerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerApplication.class, args);
+	}
+
+}
---
+++ ner-application/src/test/java/com/sriram/nerapplication/NerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sriram.nerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
